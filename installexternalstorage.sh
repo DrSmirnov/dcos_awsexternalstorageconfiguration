c11259ac3eb4cae21bb0a209dfb6f7014347748e
Tino Glässer <tglaesser@demandware.com>
2016-06-03 16:30:27 +0200

    Readd sed command

--- installexternalstorage.sh
+++ installexternalstorage.sh
@@ -29,4 +29,4 @@ cp /opt/mesosphere/etc/mesos-slave-common /home/core/backup/mesos-slave-common
 
 #Apply neccesary changes to dcos configuration
 sudo cp /home/core/resources/mesos-slave-modules.json /opt/mesosphere/etc/mesos-slave-modules.json
-#sed 's|MESOS_ISOLATION.*|MESOS_ISOLATION=cgroups/cpu,cgroups/mem,posix/disk,com_emccode_mesos_DockerVolumeDriverIsolator|' /opt/mesosphere/etc/mesos-slave-common | sudo tee /opt/mesosphere/etc/mesos-slave-common
+sed 's$MESOS_ISOLATION.*$MESOS_ISOLATION=cgroups/cpu,cgroups/mem,posix/disk,com_emccode_mesos_DockerVolumeDriverIsolator$' /opt/mesosphere/etc/mesos-slave-common | sudo tee /opt/mesosphere/etc/mesos-slave-common
